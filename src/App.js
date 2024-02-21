053e38a22ad4d42637b6ade0d7d218b9f769a60d
Anirudh <saianirudhduggineni58@gmail.com>
2024-02-21 10:20:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,11 +38,11 @@ const App = () => {
       <div>
         <nav className="navbar navbar-expand-lg bg-white sticky-top">
           <div className="container-fluid p-0">
-            <Link className="navbar-brand me-auto" to="/">
+            <a className="navbar-brand me-auto" href="https://eis-website.onrender.com/">
               <img className="ps-3" src="/Images/Subtract.png" alt="Logo" height="50px" />
               <span className="text-black-50 ms-5 fs-6 fw-bold d-none d-md-inline" style={{ fontSize: '.9rem' }}>Call US:</span>
               <span className="text-black fs-6 fw-bold d-none d-md-inline" style={{ fontSize: '.9rem' }}>+91 70326 66100</span>
-            </Link>
+            </a>
             <div className="pe-3">
               <button type="button" className="navbar-toggler border-0 text-black" onClick={handleToggleCollapse}>
                 <img src="/Images/Union.svg" alt="" />
@@ -52,32 +52,32 @@ const App = () => {
             <div className={`collapse navbar-collapse bg-white ps-3 ${isCollapsed ? 'show' : ''}`} id="navbarText" ref={collapseRef}>
               <ul className="navbar-nav position-relative mx-auto mb-2 mb-lg-0 gap-0 gap-xxl-3">
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/">Home</Link>
+                  <Link className="nav-link fw-bold text-black-50" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/aboutus">About Us</Link>
+                  <Link className="nav-link fw-bold text-black-50" to="/aboutus">About Us</Link>
                 </li>
                 <li className="nav-item dropdown">
                   <Link className="nav-link fw-bold text-black-50" to="#" aria-expanded="true">Capability <span ><i className="fa fa-angle-down ms-1 drop-icon fs-6 fw-bold" data-bs-toggle="dropdown" style={{ color: 'rgb(7, 223, 7)', fontSize: '.9rem' }} aria-hidden="true"></i></span></Link>
                   <ul className="dropdown-menu rounded-0 shadow border-0" style={{ backgroundColor: '#ffffffe7' }}>
-                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="https://eis-website.onrender.com/NPI">New Product Introduction</Link></li>
-                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="https://eis-website.onrender.com/PCB">PCB Assembly & Box Build</Link></li>
-                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="https://eis-website.onrender.com/EMF">Electronic Manufacturing</Link></li>
-                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="https://eis-website.onrender.com/product-testing">Product Testing</Link></li>
-                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="https://eis-website.onrender.com/AfterSales">After Sales & Warranty</Link></li>
+                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="/NPI">New Product Introduction</Link></li>
+                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="/PCB">PCB Assembly & Box Build</Link></li>
+                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="/EMF">Electronic Manufacturing</Link></li>
+                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="/product-testing">Product Testing</Link></li>
+                    <li className=""><Link className="dropdown-item text-black-50 fw-bold lh-lg" to="/AfterSales">After Sales & Warranty</Link></li>
                   </ul>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/CustomerLogin">Customer Portal</Link>
+                  <a className="nav-link fw-bold text-black-50" href="https://eis-website.onrender.com/CustomerLogin">Customer Portal</a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/Gallery">Gallery</Link>
+                  <a className="nav-link fw-bold text-black-50" href="https://eis-website.onrender.com/Gallery">Gallery</a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/Contactus">Contact Us</Link>
+                  <a className="nav-link fw-bold text-black-50" href="https://eis-website.onrender.com/Contactus">Contact Us</a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link fw-bold text-black-50" to="https://eis-website.onrender.com/Blog">Blog</Link>
+                  <a className="nav-link fw-bold text-black-50" href="https://eis-website.onrender.com/Blog">Blog</a>
                 </li>
               </ul>
 
@@ -95,22 +95,23 @@ const App = () => {
             </div>
           </div>
         </nav>
-        
+
         <Routes>
-          <Route path="https://eis-website.onrender.com/" element={<HomePage />} />
-          <Route path="https://eis-website.onrender.com/aboutus" element={<AboutUs />} />
-          <Route path="https://eis-website.onrender.com/product-testing" element={<ProductTesting />} />
-          <Route path="https://eis-website.onrender.com/EMF" element={<EMF />} />
-          <Route path="https://eis-website.onrender.com/NPI" element={<NPI />} />
-          <Route path="https://eis-website.onrender.com/PCB" element={<PCB />} />
-          <Route path="https://eis-website.onrender.com/AfterSales" element={<AfterSales />} />
-          <Route path="https://eis-website.onrender.com/CustomerLogin" element={<CustomerLogin />} />
-          <Route path="https://eis-website.onrender.com/ContactUS" element={<ContactUs />} />
-          <Route path="https://eis-website.onrender.com/Gallery" element={<Gallery />} />
-          <Route path="https://eis-website.onrender.com/blog" element={<Blog />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/product-testing" element={<ProductTesting />} />
+          <Route path="/EMF" element={<EMF />} />
+          <Route path="/NPI" element={<NPI />} />
+          <Route path="/PCB" element={<PCB />} />
+          <Route path="/AfterSales" element={<AfterSales />} />
+          <Route path="/CustomerLogin" element={<CustomerLogin />} />
+          <Route path="/ContactUS" element={<ContactUs />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
         </Routes>
       </div>
     </Router>
+
   );
 };
 
